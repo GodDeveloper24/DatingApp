da008ed51dfbd52153bb0015f3af11c0f171a2df
JovanJ24 <nezgodendecko@gmail.com>
2020-08-27 15:58:01 +0200

    deneskadosta e

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member';
+import {MemberEditComponent} from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { appRoutes } from './routes';
 import { tokenName } from '@angular/compiler';
 
@@ -41,6 +43,7 @@ export function tokenGetter() {
     MessagesComponent,
     MemberCardComponent,
     MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -64,6 +67,7 @@ export function tokenGetter() {
     ErrorInterceptorProvider,
     MemberDetailResolver,
     MemberListResolver,
+    MemberEditResolver
   ],
   bootstrap: [AppComponent],
 })
